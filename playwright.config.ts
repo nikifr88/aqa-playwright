a7fcd62cb77a4e6953db96132148fde7f7c67b8a
Nikita <nikifr.kh@gmail.com>
2025-10-30 20:03:59 +0200

    add env and settigns for github actions

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,9 +4,12 @@ import { defineConfig, devices } from '@playwright/test';
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
+import dotenv from 'dotenv';
+import path from 'path';
+
+dotenv.config({
+  path: path.resolve(process.cwd(), `.env.${process.env.ENV || 'stage'}`)
+});
 
 /**
  * See https://playwright.dev/docs/test-configuration.
@@ -26,7 +29,11 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    // baseURL: 'http://localhost:3000',
+    baseURL: process.env.BASE_URL,
+    httpCredentials: {
+      username: process.env.HTTP_NAME || 'guest',
+      password: process.env.HTTP_PASS || 'welcome2qauto'
+    },
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
